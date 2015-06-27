77cdd6d4f5763f6ec55615921576685162f31270
jacqueline-homan <jacquelinehoman7@gmail.com>
2015-06-27 11:35:53 -0400

    demo of websharper with three simple web pages

---
+++ WebsharperRetry/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module WebsharperRetry.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("WebsharperRetry")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("Codecatenation, LLC")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ WebsharperRetry/Program.fs
@@ -0,0 +1,49 @@
+﻿open WebSharper
+open WebSharper.Html.Server
+
+type Endpoints =
+    | [<EndPoint "GET /">] Home
+    | [<EndPoint "GET /about">] About
+    | [<EndPoint "GET /contact">] ContactUs
+
+let MySite =
+    Warp.CreateApplication (fun ctx endpoint ->
+        let (=>) label endpoint = A [HRef (ctx.Link endpoint)] -< [Text label]
+        match endpoint with
+        | Endpoints.Home ->
+            Warp.Page(
+                Body =
+                    [
+                        H1 [Text "Hello world!"]
+                        "About" => Endpoints.About
+                        H2 [Text "Welcome to the F# Web App Demo"]
+                        "Contact Us" => Endpoints.ContactUs
+                    ]
+            )
+        | Endpoints.About ->
+            Warp.Page(
+                Body = 
+                    [
+                        "Home"=> Endpoints.Home
+                        P [Text ""]
+                        "Contact" => Endpoints.ContactUs
+                        P [Text "A simple multi-page F# web app :)"]
+                        P [Text "F# is a strongly typed, type-driven language."]
+                        P [Text "As you can see, the type 'Endpoints' act like Rails' routes."]
+                    ]
+            )
+        | Endpoints.ContactUs -> 
+            Warp.Page(
+               
+                Body =
+                    [
+                        H1 [Text "Contact Us"]
+                        "Home" => Endpoints.Home
+
+
+                    ]
+            )
+    )
+
+[<EntryPoint>]
+do Warp.RunAndWaitForInput(MySite) |> ignore
